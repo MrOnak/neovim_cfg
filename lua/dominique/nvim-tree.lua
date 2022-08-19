0ec0d00cfba6d77d591c8ddca07c828ad42c9920
Dominique Stender <oldaric.gw2@gmail.com>
2022-08-19 22:33:40 +0000

    using hjkl for nvim-tree navigation

--- lua/dominique/nvim-tree.lua
+++ lua/dominique/nvim-tree.lua
@@ -1,10 +1,16 @@
 print("nvim-tree")
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
-
 if not status_ok then
   return
 end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
   disable_netrw = true,
@@ -16,6 +22,8 @@ nvim_tree.setup({
     side = "left",
     mappings = {
       list = {
+        { key = {"l", "<CR>", "o"}, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
       },
     },
   },
