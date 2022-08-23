c1328cdc0806543ad99d2e850a5a88f79ec8851f
Dominique Stender <oldaric.gw2@gmail.com>
2022-08-23 07:00:13 +0200

    added Packer bootstrap

--- lua/dominique/plugins.lua
+++ lua/dominique/plugins.lua
@@ -1,9 +1,41 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local fn = vim.fn
 
-return require('packer').startup(function(use)
+-- BOOTSTRAP Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Only required if you have packer configured as `opt`
+--vim.cmd [[packadd packer.nvim]]
+
+return packer.startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
