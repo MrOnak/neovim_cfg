6c032f4d0253bb1239e3b67e24282935f233e103
Dominique Stender <oldaric.gw2@gmail.com>
2022-09-05 10:25:07 +0200

    added border to lsp.handler.hover

--- lua/dominique/lsp/setup.lua
+++ lua/dominique/lsp/setup.lua
@@ -25,6 +25,8 @@ vim.diagnostic.config({
   }
 })
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+
 print("- Mason")
 mason.setup({
   ui = {
