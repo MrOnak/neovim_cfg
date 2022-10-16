748b6e8fd2285fb86221adf8aaf19217c1c077fd
Dominique Stender <oldaric.gw2@gmail.com>
2022-10-16 16:56:44 +0200

    changed lsp call for newer version compatibility

--- lua/dominique/lsp/handlers.lua
+++ lua/dominique/lsp/handlers.lua
@@ -41,7 +41,8 @@ local lsp_flags = {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+--capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 -- setup all language servers, supply them with above configuration
 -- ================================================================
